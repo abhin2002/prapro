f6c0e562af087684bd2ac2f76517291424a7946e
abhin2002 <abhinajith40@gmail.com>
2024-02-24 23:21:19 +0530

    plot added

--- task_10/gradient_descent.py
+++ task_10/gradient_descent.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 
@@ -13,4 +15,14 @@ for i in range(0,10):
     w = w + lr * (y[i] - yi)
 
     print("X : ",x[i], "Y : ",yi, "w : ",w)
-print("w : ",w)
+
+
+    plt.plot(x, [w * xi for xi in x], label=f"Iteration {i+1}")
+    
+
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.legend()
+plt.grid(True)
+plt.show()
